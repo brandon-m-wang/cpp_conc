60c2d2e7f0759965a3bef7a424441720f6d23d07
Brandon Wang <brandonwang0503@berkeley.edu>
2022-06-05 02:42:40 -0700

    1

---
+++ join_detach.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+
+void function_1() {
+    for (int i = 0; i < 20; i++) {
+        std::cout << "Long print." << std::endl;
+    }
+}
+
+void function_2() {
+    std::cout << "Short print." << std::endl;
+}
+
+int main() {
+    std::thread t1(function_1);
+    std::thread t2(function_2);
+    t2.detach();
+    t1.join();
+    return 0;
+}
